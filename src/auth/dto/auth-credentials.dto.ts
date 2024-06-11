3649bf93a86faf058bd5e8a656f01f7e27e8dc2e
Đoàn Minh <ititiu21243@Student.hcmiu.edu.vn>
2024-06-11 13:01:48 +0700

    Signup: hashCode Password Lecture48

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,30 @@
+import {
+  IsString,
+  Length,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @Length(8, 20, {
+    message: 'Password must be between 8 and 20 characters long.',
+  })
+  @Matches(/[A-Z]/, {
+    message: 'Password must contain at least one uppercase letter.',
+  })
+  @Matches(/[a-z]/, {
+    message: 'Password must contain at least one lowercase letter.',
+  })
+  @Matches(/[0-9]/, { message: 'Password must contain at least one digit.' })
+  @Matches(/[!@#$%^&*(),.?":{}|<>]/, {
+    message: 'Password must contain at least one special character.',
+  })
   password: string;
 }
